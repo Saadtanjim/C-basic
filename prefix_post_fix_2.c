43d8a9a7280c47e4edd46858b132b0787afd1aaf
Saadtanjim <sadtanjim@gmail.com>
2020-07-19 12:30:49 +0600

    Uh

--- prefix_post_fix_2.c
+++ prefix_post_fix_2.c
@@ -9,10 +9,12 @@ int main ()
   --b;
   c++;
   printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
-  for (int i=1; i<=5; i++)
+  for (int i=1; i<=5; ++i)
   {
     printf ("%d ", i);
   }
   printf ("\n");
+  int x = 5 + 6 * 7 / 2 - 1;
+  printf ("%d", x);
   return 0;
 }
