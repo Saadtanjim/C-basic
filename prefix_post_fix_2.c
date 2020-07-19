ab82a8192f69ea60c6f391a5e4b0e86221ca81dc
Saadtanjim <sadtanjim@gmail.com>
2020-07-19 12:21:00 +0600

    First_prefix_postfix_pronblem.. :)

---
+++ prefix_post_fix_2.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+int main ()
+{
+  int a = 5, b = 6, c = 7, d;
+  d = a++ + --b + c++;
+  --a;
+  a--;
+  b++;
+  --b;
+  c++;
+  printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+  for (int i=1; i<=5; i++)
+  {
+    printf ("%d ", i);
+  }
+  printf ("\n");
+  return 0;
+}
